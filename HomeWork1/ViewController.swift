403741ac878d37b846d675b81a18703edaa2f7f1
Uemlianin Stepan <iosdevoloper@gmail.com>
2020-05-19 18:08:57 +0200

    Initial Commit

---
+++ HomeWork1/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HomeWork1
+//
+//  Created by Stepan Uemlianin on 19/05/2020.
+//  Copyright © 2020 Stepan Uemlianin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
